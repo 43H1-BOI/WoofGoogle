9239adb4d50b28381c2ab70a8dcb1c233b46e961
Abhishek Yadav <153166442+43H1-BOI@users.noreply.github.com>
2025-07-28 22:05:31 +0530

    Added Preview Function

--- app/src/main/java/com/the43h1/woofgoogle/MainActivity.kt
+++ app/src/main/java/com/the43h1/woofgoogle/MainActivity.kt
@@ -31,17 +31,70 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun DogCard(dogData: DogData, modifier: Modifier = Modifier) {
+    Card(
         modifier = modifier
-    )
+            .padding(8.dp)
+            .fillMaxWidth(),
+        shape = RoundedCornerShape(0.dp, 12.dp, 0.dp, 12.dp)
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .padding(8.dp)
+                .heightIn(max = 100.dp)
+                .fillMaxWidth()
+        ) {
+            Image(
+                painter = painterResource(dogData.image),
+                contentDescription = stringResource(dogData.name),
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .size(50.dp)
+                    .clip(CircleShape)
+                    .weight(1f)
+            )
+            Text(
+                buildAnnotatedString {
+                    withStyle(style = SpanStyle()) {
+                        append(stringResource(dogData.name))
+                        append("\n${dogData.age} ${stringResource(R.string.years_old)}")
+                    }
+                },
+                modifier = Modifier.weight(5f)
+
+            )
+            IconButton(
+                onClick = {},
+                modifier = Modifier.weight(1f)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.KeyboardArrowDown,
+                    contentDescription = "Show More"
+                )
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     WoofGoogleTheme {
-        Greeting("Android")
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            DogCard(
+                DogData(
+                    image = R.drawable.dog_1_koda,
+                    name = R.string.dog_name_1,
+                    about = R.string.dog_description_1,
+                    age = 8
+                )
+            )
+        }
     }
 }
