e864bbf419e6d881ae2f4e042e4b1e3b05a9a37a
Abhishek Yadav <153166442+43H1-BOI@users.noreply.github.com>
2025-07-30 13:26:56 +0530

    Added Animation

--- app/src/main/java/com/the43h1/woofgoogle/MainActivity.kt
+++ app/src/main/java/com/the43h1/woofgoogle/MainActivity.kt
@@ -7,6 +7,10 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
